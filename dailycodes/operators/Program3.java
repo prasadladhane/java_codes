65ca0ea15010f85d65c4675a8dcb2fa7298b68f4
prasadladhane <prasadladhane123@gmail.com>
2026-04-15 23:13:45 +0530

    operator revision

---
+++ dailycodes/operators/Program3.java
@@ -0,0 +1,14 @@
+class Demo3{
+	public static void main(String[]args){
+		//Relational Operators
+		int x=10;
+		int y=7;
+
+		System.out.println(x>y);
+		System.out.println(x>=y);
+		System.out.println(x<y);
+		System.out.println(x>=y);
+		System.out.println(x==y);
+		System.out.println(x!=y);
+	}
+}
