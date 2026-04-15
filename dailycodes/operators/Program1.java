ad96029badb7413f13daf70c2bac4952ba2f360d
prasadladhane <prasadladhane123@gmail.com>
2026-04-15 23:04:18 +0530

    operator revision-arithmetic

---
+++ dailycodes/operators/Program1.java
@@ -0,0 +1,13 @@
+class Demo1{
+	public static void main(String[]args){
+		//Arithmetic Operators revision
+		int num1=7;
+		int num2=5;
+
+		System.out.println("Addition:" + (num1+num2));
+		System.out.println("Substraction:" + (num1-num2));
+		System.out.println("Multiplication:" + (num1*num2));
+		System.out.println("Division:" + (num1/num2));
+
+	}
+}
