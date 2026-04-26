fb7a9098ba5705a8dea1bbd3d5ae2f0dcd6a7303
prasadladhane <prasadladhane123@gmail.com>
2026-04-26 10:41:10 +0530

    triangle pattern 1

---
+++ dailycodes/loops/pattern_codes/trianglePattern_1/Program8.java
@@ -0,0 +1,11 @@
+class Demo8{
+	public static void main(String[]args){
+		int n=5;
+		for(int i=n;i>=1;i--){
+			for(int j=n;j>=i;j--){
+				System.out.print((char)(96+i)+" ");
+			}
+			System.out.println();
+		}
+	}
+}
