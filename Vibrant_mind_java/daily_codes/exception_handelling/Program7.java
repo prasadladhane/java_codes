d748e74b7897ef610c516bcd7e2b4e7973c67f24
prasadladhane <prasadladhane123@gmail.com>
2026-06-14 10:10:27 +0530

    exception handelling

--- Vibrant_mind_java/daily_codes/exception_handelling/Program7.java
+++ Vibrant_mind_java/daily_codes/exception_handelling/Program7.java
@@ -1,1 +1,26 @@
-dhhd
+import java.io.*;
+
+class Demo7{
+	public static void readFile(String path){
+		FileReader fr= null;
+
+		try{
+			fr=new FileReader(path);
+
+		}catch(FileNotFoundException e){
+
+			System.out.println("File not Found!");
+
+		}finally{
+			if(fr!=null){
+				fr.close();
+			}
+		}
+	}
+}
+class Client{
+	public static void main(String[]args){
+		Demo7.readFile("java/VibrantMind_Java/daily_codes/");
+	}
+
+}
