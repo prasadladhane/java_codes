c1d4ba57a41138004d1950d9b0eb06157cc96d7e
prasadladhane <prasadladhane123@gmail.com>
2026-07-15 20:04:27 +0530

    for each loop

---
+++ Vibrant_mind_java/daily_codes/collection_framework/1-Iterable/Program1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class ForEachDemo{
+	public static void main(String[]args){
+		
+		ArrayList al=new ArrayList();
+
+		//ArrayList <Integer> al=new ArrayList <Integer> ();
+		
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		System.out.println(al);
+
+		//for(Integer x:al){
+		for(Object x:al){
+			System.out.println(x);
+		}
+	}
+}
+
