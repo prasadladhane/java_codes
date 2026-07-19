0cef0d7a04d5ffefb5a739ba3fbf47d01ce0cdea
prasadladhane <prasadladhane123@gmail.com>
2026-07-19 12:53:50 +0530

    list code

---
+++ Vibrant_mind_java/daily_codes/collection_framework/3-List/Program1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class ArrayListDemo{
+	public static void main(String[]args){
+		List al=new ArrayList();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		System.out.println(al);
+	}
+}
+
